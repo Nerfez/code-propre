3a9d847ef127d14713d594c1b0e6c41556eee3ad
Clement Olivier <oliclems@gmail.com>
2023-03-27 15:39:23 +0200

    feat(tp): ajout tp01

---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,23 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * Classe qui permet de créer une entreprise
+ */
+public class Entreprise {
+
+	public int siret;
+	public String nom;
+	public String adresse;
+	public Date date_Creation;
+	public static final int CAPITAL_MAX = 3000000;
+
+	/**
+	 * Méthode qui affiche le Statut
+	 */
+	public void afficherStatut(){
+		//Affichage
+	}
+	
+}
